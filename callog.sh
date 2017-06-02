0b623ccb31d9dd1754892a5ce7ef44a6b41e4f85
WJHPrince <120709527@qq.com>
2017-06-02 16:31:03 +0800

    这是筛选访问最多的IP及其访问路径的

---
+++ callog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat access.log | awk '{a[$1]+=1;}END{for(i in a){print a[i]" " i;}}' | sort -g > ip.log
+tail -n -1 ip.log | awk '{print $2}' | tee ip.most
+echo '-----------------ip logs------------------------------'
+read IPMOST < ip.most
+cat access.log | grep "$IPMOST" | awk '{print $7}' | tee ip.most.logs
